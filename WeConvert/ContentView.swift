43876179da0dbf23ea035f309a8bcb09fad5b1be
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-08-05 17:21:25 +0530

    Initial Commit

---
+++ WeConvert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeConvert
+//
+//  Created by Nimrat Khalsa on 05/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Nimrat Khalsa on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
